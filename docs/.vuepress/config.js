bbb30a7683606cc644c8248255f514f7ad0f8886
rakeden <5896201+rakeden@users.noreply.github.com>
2023-06-02 14:49:58 +0200

    adding custom project and client config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'WAX Developer Portal',
+  description: 'general documentation for the WAX Ecosystem',
+})
